c5e38080d3aa1aea288e98d1a5472f20db18dedd
Al-Amin hossain <252310005101005@stamford.university>
2025-04-29 23:33:31 +0600

    Remove bottom border from container in LandingPage for cleaner appearance

--- src/components/landing/LandingPage.tsx
+++ src/components/landing/LandingPage.tsx
@@ -328,7 +328,7 @@ const LandingPage: React.FC<LandingPageProps> = ({ formRef }) => {
         </HeroContentWrapper>
       </HeroSection>
 
-      <Container maxWidth="lg">
+      <Container maxWidth="lg">  {/* Bottom Border Removed */}
         <Box sx={{ my: 5 }}>
           <Grid container spacing={isMobile ? 3 : 4}>
             <Grid component="div" item xs={12} md={5}>
@@ -342,7 +342,8 @@ const LandingPage: React.FC<LandingPageProps> = ({ formRef }) => {
                   sx={{
                     fontWeight: 600,
                     fontSize: isMobile ? "1.8rem" : "2.2rem",
-                    borderBottom: `2px solid ${colors.light}`,
+                    // borderBottom: `2px solid ${colors.light}`,
+                    borderBottom: "none",
                     paddingBottom: 1,
                     marginBottom: 3,
                     width: "100%",
